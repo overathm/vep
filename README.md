317e2485936b78c0d214acb63e37d68cc5bd9583
Momo <s6mmover@uni-bonn.de>
2021-09-09 12:17:54 +0200

    some content for the README.md
    now the script find files recursive

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # vep
+
+Instructions:
+the script can be used without root rights. I would also highly recommend this. the user just has to be added to the docker group first with:
+sudo usermod -a -G docker $(whoami)
+
+make a new folder:
+mkdir foldername
+
+copy the script into that folder:
+https://github.com/overathm/vep
+
+make the script executable:
+chmod +x Run_VEP_batch_v0_5.sh
+
+and execute it with:
+./Run_VEP_batch_v0_5.sh
+
+After that the script will be executed hourly.
+
+The following programmes are required:
+wget tar docker pgrep gzip bgzip
+and at least 30GB of memory is required.
+However, the progam indicates that there is not enough memory and that programs are not installed.
--- Run_VEP_batch_v0_5.sh
+++ Run_VEP_batch_v0_5.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+
 #The idea would be to go through the input folder and fetch the .vcf files to be annotated by VEP, as disscused, important would be
 #that the files who were already analyzed are renamed or moved to a different location to avoid reanalysis
 
-#here be dragons! Don't change anything!
+
+#here is the dragon! Don't change anything!
 #variable that specifies the time how long files have to remain in the input folder
 waitperiod=5
 #name of the directories which we will need
 dir=(input output reference archive archive/logs)
-needed_commands=(wget tar docker pgrep gzip bgzip )
+needed_commands=(wget tar docker)
 #name of reference file
 refseq="homo_sapiens_refseq_vep_104_GRCh37"
 #name of the fasta file
@@ -17,10 +19,7 @@ fasta="Homo_sapiens.GRCh37.dna.primary_assembly"
 docker_name="vep_nngm"
 #dir of mounting
 mount_dir="/opt/vep/.vep"
-#name of the script
-scriptname=$(basename "$0")
 
-#check if the user have the correct permissions
 if [ $(groups $(whoami) | grep  'docker'|wc -l)  -eq 0 ];then
   if [ $(id -u) -ne 0 ];then
    printf "ERROR: You are not in the docker group, nor are you running this program with root privileges!!!!\nto add yourself to the docker group you can execute the following command:\nusermod -a -G docker $(whoami).\nEXIT\n"
@@ -38,7 +37,7 @@ for i in "${needed_commands[@]}";do
 done
 
 #check if docker is running
-if [ $(systemctl is-active docker) == "active" ];then
+if [ ! $(pgrep -f docker|wc -l) -ne 1 ];then
     printf "docker is installed and running\n"
 else
     printf  "ERROR: docker is not running, please check!\n EXIT!!!\n"
@@ -51,13 +50,14 @@ for i in "${dir[@]}";do
         mkdir -p $i
             if [ $? -eq 0 ];then
 	        printf "Folder $i created\n"
-                chmod a+rwx $i	
+                chmod a+rwx $i
             else
                 printf "ERROR: there is a problem with creating folder $i\nEXIT\n"
                 exit
             fi
     fi
 done
+
 
 #check if refseq exists otherwise download it
 if [ ! -f "reference/${refseq}.tar.gz" ]; then
@@ -70,27 +70,15 @@ if [ ! -f "reference/${refseq}.tar.gz" ]; then
   fi
 fi
 
-#check if fasta exists otherwise download it
 if [ ! -f "reference/${fasta}.fa.gz" ]; then
   if [ $(df -P . | tail -1 | xargs | cut -d" " -f4) -lt 1000000 ];then
     printf "ERROR: There is not enough diskspace for downloading the FASTA file\nEXIT\n"
     exit
   else
     wget -P reference http://ftp.ensembl.org/pub/grch37/current/fasta/homo_sapiens/dna/${fasta}.fa.gz
-    gzip -d reference/${fasta}.fa.gz > reference/${fasta}.fa
-    bgzip -c reference/${fasta}.fa > reference/${fasta}.fa.gz
+    #gzip -d reference/${fasta}.fa.gz > reference/${fasta}.fa
+    #bgzip reference/${fasta}.fa > reference/${fasta}
   fi
-fi
-
-#make cronjob if not existing
-if [ $(crontab -l | grep $scriptname | wc -l) -eq 0 ];then
-  crontab -l > vepcron
-  #echo new cron into cron file
-  echo "22 * * * * cd $(pwd) && ./$scriptname >> $(pwd)/archive/logs/$(date +"%Y%m%d")cron.log 2>&1" >> vepcron
-  cat vepcron
-  #install new cron file
-  crontab vepcron
-  rm vepcron
 fi
 
 #pull the right docker image
@@ -99,6 +87,7 @@ docker pull ensemblorg/ensembl-vep:release_104.3
 #run docker image
 docker_con=$( docker run -d -i -t -v $(pwd):${mount_dir} ensemblorg/ensembl-vep )
 printf "docker image with the ID $docker_con is started\n"
+
 
 #check if there are files in the input
 if [ $(ls input/ | grep .*.vcf | wc -l) -eq 0 ];then
@@ -110,22 +99,16 @@ if [ $(df -P . | tail -1 | xargs | cut -d" " -f4) -lt 5000000 ];then
   printf "INFO: There is less then 5GB space left on your disk, please check\n"
 fi
 
-#check for whitespaces and repaces them with underscore
-for f in input/*\ *
-do 
-  mv "$f" "${f// /_}" 
-  printf "I don't like whitespaces: $f is renamed to ${f// /_}"
-done
 
 #for every file that ends with .vfc that stayed ther for longer than timeout (that value is stored in the config)
-for i in $(find input/*.vcf -mmin +$waitperiod) 
+  for i in $(find input/*.vcf -mmin +$waitperiod)
   do
     BASENAME=$( echo $i | cut -d'/' -f2)
-    out_name=${BASENAME}_VEP_RefSeq.vcf	
- 
-    cp $i archive 
+    out_name=${BASENAME}_VEP_RefSeq.vcf
+
+    cp $i archive
     if [ $? -eq 0 ];then
-      printf "file $i is copied to archive\n"	
+      printf "file $i is copied to archive\n"
     else
       printf "ERROR: the file $i cannot be copied into the archive\nEXIT\n"
       exit
@@ -136,7 +119,7 @@ for i in $(find input/*.vcf -mmin +$waitperiod)
     #during the run or a .log file
 
 
-    (docker exec -i ${docker_con} /bin/bash -c "./vep\
+    (docker exec -it ${docker_con} ./vep \
     --offline \
     --hgvs \
     --fasta ${mount_dir}/reference/${fasta}.fa.gz\
@@ -145,9 +128,9 @@ for i in $(find input/*.vcf -mmin +$waitperiod)
     --format vcf \
     --vcf \
     --force_overwrite \
-    --dir_cache ${mount_dir}/reference \
+    --dir_cache ${mount_dir}/reference/ \
     --input_file ${mount_dir}/$i \
-    -output_file ${mount_dir}/output/$out_name ; chmod 666 ${mount_dir}/output/*" ) >> archive/logs/$(date +"%Y%m%d")docker.log
+    -output_file ${mount_dir}/output/$out_name) >> archive/logs/$(date +"%Y%m%d")dockerlogs.txt
 
     if [ -f archive/$BASENAME ] && [ -f output/$out_name ];then
       rm $i
